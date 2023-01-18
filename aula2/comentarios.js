92f058aa8453791e0ad7808c095eab0a48de4677
Lucas D <nightdavila@gmail.com>
2023-01-17 21:27:46 -0300

    Adicionado comentarios para ilustriar como sao feitos os comentarios em JS

---
+++ aula2/comentarios.js
@@ -0,0 +1,13 @@
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+
+/* 
+Comentario de bloco
+Linha de comentário
+*/
+
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado 
