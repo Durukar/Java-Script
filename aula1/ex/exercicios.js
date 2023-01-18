bab84c0f5ba13ccb261767e64062bfec4ec1c27e
Lucas D <nightdavila@gmail.com>
2023-01-17 21:15:02 -0300

    Adicionado meu nome no campo NAME

--- aula1/ex/exercicios.js
+++ aula1/ex/exercicios.js
@@ -1,4 +1,4 @@
 // Exiba a frase:
 // Meu nome é "NOME". Estou aprendendo JavaScript às 9 da noite.
 
-console.log('Meu nome é "NOME". Estou aprendendo JavaScript às 9 da noite.')
+console.log('Meu nome é "Lucas". Estou aprendendo JavaScript às', 9, 'da noite.');
