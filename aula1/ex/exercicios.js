ab42ca2d7a71377eff05a58b34cf111f5e8ba3fd
Lucas D <nightdavila@gmail.com>
2023-01-17 21:12:05 -0300

    Adicionado Number em vez de INT no comentario

---
+++ aula1/ex/exercicios.js
@@ -0,0 +1,4 @@
+// Exiba a frase:
+// Meu nome é "NOME". Estou aprendendo JavaScript às 9 da noite.
+
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às 9 da noite.')
--- aula1/index.js
+++ aula1/index.js
@@ -2,5 +2,5 @@ console.log('Hello, "World"!'); // String = Texto
 console.log("Hello, 'World'!");  
 console.log(`'Hello', "World"!`);
 
-console.log(123456); // Int = Numeros
+console.log(123456); // Number = Número
 console.log(15.85);
