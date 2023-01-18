d3fe1d061e2311a39b374422eb66c5f0d3d60502
Lucas D <nightdavila@gmail.com>
2023-01-17 21:07:10 -0300

    Adicionado ints no console log

--- aula1/index.js
+++ aula1/index.js
@@ -1,3 +1,6 @@
 console.log('Hello, "World"!'); // String = Texto
-console.log("Hello, 'World'!"); 
+console.log("Hello, 'World'!");  
 console.log(`'Hello', "World"!`);
+
+console.log(123456); // Int = Numeros
+console.log(15.85);
